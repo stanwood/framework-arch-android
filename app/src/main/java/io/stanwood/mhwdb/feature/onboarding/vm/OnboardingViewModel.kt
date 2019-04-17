a36200a009045c48e68e35fa5aa840ca21b21155
Sven Bendel <ubuntudroid@gmail.com>
2019-04-17 17:00:37 +0200

    SFW-332 Add support for navigation via destination res ID as well as back and up

--- app/src/main/java/io/stanwood/mhwdb/feature/onboarding/vm/OnboardingViewModel.kt
+++ app/src/main/java/io/stanwood/mhwdb/feature/onboarding/vm/OnboardingViewModel.kt
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2019 stanwood GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.stanwood.mhwdb.feature.onboarding.vm
 
 import io.reactivex.Observable
@@ -6,6 +27,7 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.PublishSubject
 import io.stanwood.framework.arch.core.ViewModel
+import io.stanwood.framework.arch.nav.Direction
 import io.stanwood.framework.arch.nav.NavigationTarget
 import io.stanwood.mhwdb.feature.main.dataprovider.MainDataProvider
 import io.stanwood.mhwdb.feature.onboarding.ui.OnboardingFragmentDirections
@@ -23,7 +45,7 @@ class OnboardingViewModel @Inject constructor(private val dataProvider: MainData
                 .subscribeBy(
                     onError = { it.printStackTrace() },
                     onComplete = {
-                        navigation.onNext(NavigationTarget(OnboardingFragmentDirections.actionToContainerFragment()))
+                        navigation.onNext(Direction(OnboardingFragmentDirections.actionToContainerFragment()))
                     })
         }
     }
